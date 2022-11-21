71aba49a09a88a3ab5edcc730667aca985f96418
jaszczomp71 <smietnizdupy@gmail.com>
2022-11-22 00:58:25 +0100

    brak komunikatu

--- APItrening/app.js
+++ APItrening/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function(req, res) {
+
+    https get.
+
+
     res.send("Server is up and running.")
 })
 
