c35041273c5b378b93c07c32725135ae57a26f8a
jaszczomp71 <smietnizdupy@gmail.com>
2022-11-22 20:13:26 +0100

    day2

--- APItrening/app.js
+++ APItrening/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const https = require("https");
+const url = "https://api.openweathermap.org/data/3.0/onecall?lat=51.10&lon=17.03&exclude=hourly,daily&units=metric&appid=48fa62bf5456579753cc1fa070053ead"
 
 const app = express();
 
 app.get("/", function(req, res) {
 
-    https get.
+    https.get(url,function(response){
+        console.log(response);
+    })
 
 
     res.send("Server is up and running.")
